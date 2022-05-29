a65ceabc6fadfac27d16838c7d45a8b063475cf6
GoAlive13 <95854153+saitej98@users.noreply.github.com>
2022-05-29 23:22:29 +0530

    final

--- syngenta/src/Components/Balloon.jsx
+++ syngenta/src/Components/Balloon.jsx
@@ -1,36 +1,75 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { getColors } from "./getColors";
 function Balloon() {
-  let colorref = useRef();
+  //for colors
   let [color, setColor] = useState([]);
 
+  // for individual balloon
+
+  let [key, setKey] = useState(false);
+
+  //for shooted balloon
+
+  let [empty, setEmpty] = useState([]);
+
   useEffect(() => {
-    setColor(getColors());
+    ColorsArray();
   }, []);
 
-    const handleSubmit = () => {
-        // document.querySelectorAll(".colorballoons").style.color="white"
+  const ColorsArray = () => {
+    const balloons = Array(5)
+      .fill()
+      .map((_, i) => ({ id: i, color: getColors(i) }));
+    setColor([...balloons]);
+  };
+
+  const shoot = (key) => {
+    if (key != false) {
+      let Array = color;
+      let pop = Array.splice(key - 1, 1);
+      empty.push(...pop);
+      setKey(null);
+      setColor([...color]);
+      document.getElementById("number").value = "";
     }
+  };
+
+  const shootedballon = (key) => {
+    let Array = empty;
+    let pop = Array.splice(key, 1);
+    color.push(...pop);
+    color.sort(function (a, b) {
+      return a.id - b.id;
+    });
+    setColor([...color]);
+  };
+
   return (
     <div className="container">
       <h1 style={{ textAlign: "center" }}>Balloon Assignment</h1>
       <div className="outercontainer">
         <div className="right">
-          {color.map((elem, i) => (
+          {color.map((elem) => (
             <div
               className="colorballoons"
-              id={i + 1}
-              key={i}
-              style={{ backgroundColor: `${elem}` }}
-            >
-            </div>
+              key={elem.id}
+              style={{ backgroundColor: elem.color }}
+            ></div>
           ))}
         </div>
         <div className="left">
-          <div className="emptydiv"></div>
+          <div className="emptydiv">
+            {empty.map((el, index) => (
+              <div
+                onClick={() => shootedballon(index)}
+                key={el.id}
+                style={{ backgroundColor: el.color }}
+              ></div>
+            ))}
+          </div>
           <div className="shoot">
             <input
-              ref={colorref}
+              id="number"
               type="telephone"
               placeholder="Enter Number(1-5)"
               maxLength="1"
@@ -38,15 +77,10 @@ function Balloon() {
                 marginTop: "25%",
                 fontSize: "1rem",
               }}
+              onChange={(e) => setKey(e.target.value)}
             />
             <br />
-            <input
-              type="button"
-              onClick={handleSubmit()}
-              // console.log(colorref.current.value);
-
-              value="Shoot"
-            />
+            <input type="button" onClick={() => shoot(key)} value="Shoot" />
           </div>
         </div>
       </div>
--- syngenta/src/Components/getColors.jsx
+++ syngenta/src/Components/getColors.jsx
@@ -1,12 +1,11 @@
 import { RandomColor } from "./RandomColor";
-export const getColors = () => {
+export const getColors = (x) => {
   let Array = [];
   let colors = ["#ff0000", "#0000ff", "#3cb371", "#ee82ee", "#ffa500"];
   for (let i = 0; i < 5; i++) {
     let prop = RandomColor(0, colors.length - 1);
-    Array.push(colors[prop]);
-
-    colors.splice(prop, 1);
+    Array.push([colors[prop]]);
     }
-  return Array;
+  return Array[x];
+
 };
