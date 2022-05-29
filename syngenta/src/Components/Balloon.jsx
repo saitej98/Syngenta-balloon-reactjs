b6c07ef857250976a1e2383e44fa9fd03615eb5c
GoAlive13 <95854153+saitej98@users.noreply.github.com>
2022-05-29 19:44:57 +0530

    changes

--- syngenta/src/Components/Balloon.jsx
+++ syngenta/src/Components/Balloon.jsx
@@ -1,31 +1,52 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { getColors } from "./getColors";
 function Balloon() {
+  let colorref = useRef();
   let [color, setColor] = useState([]);
 
   useEffect(() => {
-   console.log (setColor(getColors()));
+    setColor(getColors());
   }, []);
 
+    const handleSubmit = () => {
+        // document.querySelectorAll(".colorballoons").style.color="white"
+    }
   return (
     <div className="container">
       <h1 style={{ textAlign: "center" }}>Balloon Assignment</h1>
       <div className="outercontainer">
-              <div className="right">{color.map((elem) => (
-                  <div style={{ backgroundColor: `${elem}` }}></div>
-              ))} </div>
+        <div className="right">
+          {color.map((elem, i) => (
+            <div
+              className="colorballoons"
+              id={i + 1}
+              key={i}
+              style={{ backgroundColor: `${elem}` }}
+            >
+            </div>
+          ))}
+        </div>
         <div className="left">
           <div className="emptydiv"></div>
           <div className="shoot">
             <input
-              type="number"
+              ref={colorref}
+              type="telephone"
+              placeholder="Enter Number(1-5)"
+              maxLength="1"
               style={{
                 marginTop: "25%",
                 fontSize: "1rem",
               }}
             />
             <br />
-            <input type="button" value="Shoot" />
+            <input
+              type="button"
+              onClick={handleSubmit()}
+              // console.log(colorref.current.value);
+
+              value="Shoot"
+            />
           </div>
         </div>
       </div>
--- syngenta/src/Components/RandomColor.jsx
+++ syngenta/src/Components/RandomColor.jsx
@@ -2,7 +2,6 @@ export const RandomColor = (x, y) => {
   let a = y - x + 1;
   let b = Math.random() * a;
   let output = Math.floor(b) + x;
-  console.log(output);
     return output;
   
 };
